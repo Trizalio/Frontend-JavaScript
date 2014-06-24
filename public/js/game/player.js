5ec4d9cdfd90e2d606bb9ce16a3a8d077205348c
Trizalio <gifla@ya.ru>
2014-06-24 07:30:47 +0400

    medium point

--- public/js/game/player.js
+++ public/js/game/player.js
@@ -8,6 +8,9 @@ define([
     var Player = Pilot.$extend ( {
         __init__ : function(ship, side) {
             this.$super(ship, side);
+            ship.canonsGroup1 = [];
+            ship.canonsGroup2 = [];
+            ship.canonsGroup3 = [];
         },
         turn : function () {
             if(this.ship){
@@ -29,6 +32,27 @@ define([
         move : function () {
             //console.log("hi");
             var backwardsPower = 0.3;
+            for (var i = 0; i < this.ship.canonsGroup1.length; i++){
+                if(game.keydown["x"] == true){
+                    this.ship.canonsGroup1[i].shoot = true;
+                }else{
+                    this.ship.canonsGroup1[i].shoot = false;
+                }
+            }
+            for (var i = 0; i < this.ship.canonsGroup2.length; i++){
+                if(game.keydown["c"] == true){
+                    this.ship.canonsGroup2[i].shoot = true;
+                }else{
+                    this.ship.canonsGroup2[i].shoot = false;
+                }
+            }
+            for (var i = 0; i < this.ship.canonsGroup3.length; i++){
+                if(game.keydown["v"] == true){
+                    this.ship.canonsGroup3[i].shoot = true;
+                }else{
+                    this.ship.canonsGroup3[i].shoot = false;
+                }
+            }
             if(game.keydown["w"] == true){
                 this.ship.vx += this.ship.velocity*Math.sin(this.ship.angle);
                 this.ship.vy -= this.ship.velocity*Math.cos(this.ship.angle);
